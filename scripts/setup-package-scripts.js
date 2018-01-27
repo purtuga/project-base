6d56770fbde12218a71e2733b9be3c4affc28f92
Paul T <paultavares1@gmail.com>
2018-01-27 18:00:33 -0500

    script to add default scripts to package.json

---
+++ scripts/setup-package-scripts.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+const cwd = process.cwd();
+const promisify = require("util").promisify;
+const writeFile = promisify(fs.writeFile);
+const readline = require('readline');
+const packageJsonFile = path.join(cwd, "package.json");
+const packageJson = require(packageJsonFile);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question(`
+
+This will replace your existing package.json script section. 
+
+Continue? Y|n  `, answer => {
+    answer = String(answer).trim().toLowerCase();
+    if (answer === "y" || !answer) {
+        packageJson.scripts = {
+            "serve": "webpack-dev-server --config node_modules/Base/configs/webpack.dev.js --progress --hot --color --entry ./dev/index.js",
+            "build": "webpack --config node_modules/Base/configs/webpack.dev.js",
+            "build:prod": "webpack --config node_modules/Base/configs/webpack.prod.js",
+            "build:prod:min": "webpack --config node_modules/Base/configs/webpack.prod.uglify.js",
+            "dist": "npm run build:prod&&npm run build:prod:min",
+            "postinstall": "node node_modules/Base/scripts/create-dev"
+        };
+
+        writeFile(packageJsonFile, JSON.stringify(packageJson, null, 4)).then(() => {
+            console.log(`
+    Done!
+    Review package.json for change made.
+`);
+            process.exit(0);
+        });
+    }
+    else {
+        console.log(`
+Aborted. Nothing done!
+`);
+        process.exit(0);
+    }
+});
+
+
+
+
+
