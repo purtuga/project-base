491e31fec2af7fd6c4a6df1bea76b17c6cd91d22
Paul T <paultavares1@gmail.com>
2018-01-27 17:44:49 -0500

    Added create-dev script + template

---
+++ scripts/create-dev.js
@@ -0,0 +1,55 @@
+//-----------------------------------------------------------------------------
+// Creates a "dev" folder good for development of libraries.
+//
+// To use it, add the following to package.json scripts:
+//
+//      {
+//          scripts: {
+//              "postinstall": "node node_modules/Base/scripts/create-dev"
+//          }
+//      }
+//
+//-----------------------------------------------------------------------------
+const promisify     = require("util").promisify;
+const fs            = require("fs");
+const path          = require("path");
+const CWD           = process.cwd();
+const buildOutFile  = require("../configs/webpack.dev").output.filename;
+const devDir        = path.join(CWD, "dev");
+const templateLoc   = path.join(__dirname, "..", "templates", "dev");
+const readDir       = promisify(fs.readdir);
+const readFile      = promisify(fs.readFile);
+const writeFile     = promisify(fs.writeFile);
+const mkdir         = promisify(fs.mkdir);
+
+if (fs.existsSync(devDir)) {
+    console.log(`
+
+    Unable to create "dev" folder - it already exists.
+    ${ devDir }
+    Exiting.
+
+`);
+    process.exit(0);
+}
+
+console.log("copy template from: " + templateLoc);
+
+mkdir(devDir)
+    .then(() => readDir(templateLoc))
+    .then(files => {
+        return files.reduce((whenPriorCompletes, fileName) => {
+            return whenPriorCompletes
+                .then(() => {
+                    return readFile(path.join(templateLoc, fileName), "utf8")
+                        .then(fileContent => {
+                            if (fileName === "index.html") {
+                                fileContent = fileContent.replace("_____PACKAGE_NAME_____", buildOutFile);
+                            }
+                            return writeFile(path.join(devDir, fileName), fileContent);
+                        });
+                })
+        }, Promise.resolve());
+    })
+    .catch(console.error);
+
