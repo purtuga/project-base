755ad9f7b27bf7548c1475a399a2ab06288b4210
Paul T <paultavares1@gmail.com>
2018-11-11 14:43:10 -0500

    Bug fix to package scripts

--- scripts/setup-package-scripts.js
+++ scripts/setup-package-scripts.js
@@ -25,41 +25,41 @@ Continue? Y|n  `, answer => {
     answer = String(answer).trim().toLowerCase();
     if (answer === "y" || !answer) {
         packageJson.scripts = {
-            "serve": "webpack-dev-server --config node_modules/project-base/configs/webpack.config.js --progress --hot --color --entry ./my.dev/index.js",
-            "serve:test": "webpack-dev-server --config node_modules/project-base/configs/webpack.config.js --entry ./test/index.js --output-path ./my.dev --output-filename test.bundle.js",
+            "serve": "webpack-dev-server --config node_modules/@purtuga/project-base/configs/webpack.config.js --progress --hot --color --entry ./my.dev/index.js",
+            "serve:test": "webpack-dev-server --config node_modules/@purtuga/project-base/configs/webpack.config.js --entry ./test/index.js --output-path ./my.dev --output-filename test.bundle.js",
 
-            "build": "webpack --config node_modules/project-base/configs/webpack.config.js",
-            "build:ie": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.legacy.non-minified --entry ./my.dev/index.js --output-path ./my.dev --output-filename ie-test-bundle.js",
+            "build": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js",
+            "build:ie": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.legacy.non-minified --entry ./my.dev/index.js --output-path ./my.dev --output-filename ie-test-bundle.js",
 
-            "build:prod": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.minified --env.build=prod.non-minified",
-            "build:prod:min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.minified",
-            "build:prod:non-min": "webpack --config node_modules/project-base/configs/webpack.config.js --evn.build=prod.non-minified",
+            "build:prod": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.minified --env.build=prod.non-minified",
+            "build:prod:min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.minified",
+            "build:prod:non-min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --evn.build=prod.non-minified",
 
-            "build:prod:legacy": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.legacy.minified --env.build=prod.legacy.non-minified",
-            "build:prod:legacy:min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.legacy.minified",
-            "build:prod:legacy:non-min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.legacy.non-minified",
+            "build:prod:legacy": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.legacy.minified --env.build=prod.legacy.non-minified",
+            "build:prod:legacy:min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.legacy.minified",
+            "build:prod:legacy:non-min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.legacy.non-minified",
 
-            "build:prod:esm": "webpack --config node_modules/project-base/configs/webpack.prod.esm.js --env.build=prod.esm.minified --env.build=prod.esm.non-minified",
-            "build:prod:esm:min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.esm.minified",
-            "build:prod:esm:non-min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.esm.non-minified",
+            "build:prod:esm": "webpack --config node_modules/@purtuga/project-base/configs/webpack.prod.esm.js --env.build=prod.esm.minified --env.build=prod.esm.non-minified",
+            "build:prod:esm:min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.esm.minified",
+            "build:prod:esm:non-min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.esm.non-minified",
 
-            "build:prod:wc": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.wc.minified --env.build=prod.wc.non-minified",
-            "build:prod:wc:min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.wc.minified",
-            "build:prod:wc:non-min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.wc.non-minified",
+            "build:prod:wc": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.wc.minified --env.build=prod.wc.non-minified",
+            "build:prod:wc:min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.wc.minified",
+            "build:prod:wc:non-min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.wc.non-minified",
 
-            "build:prod:wc:legacy": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.wc.legacy.minified --env.build=prod.wc.legacy.non-minified",
-            "build:prod:wc:legacy:min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.wc.legacy.minified",
-            "build:prod:wc:legacy:non-min": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.wc.legacy.non-minified",
+            "build:prod:wc:legacy": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.wc.legacy.minified --env.build=prod.wc.legacy.non-minified",
+            "build:prod:wc:legacy:min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.wc.legacy.minified",
+            "build:prod:wc:legacy:non-min": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.wc.legacy.non-minified",
 
-            "build:apiDocs": "jsdoc -c node_modules/project-base/configs/jsdoc.conf.json",
-            "build:test": "webpack --config node_modules/project-base/configs/webpack.config.js --entry ./test/index.js --output-path ./my.dev --output-filename test.bundle.js",
+            "build:apiDocs": "jsdoc -c node_modules/@purtuga/project-base/configs/jsdoc.conf.json",
+            "build:test": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --entry ./test/index.js --output-path ./my.dev --output-filename test.bundle.js",
 
-            "dist": "webpack --config node_modules/project-base/configs/webpack.config.js --env.build=prod.minified --env.build=prod.legacy.minified --env.build=prod.esm.minified",
+            "dist": "webpack --config node_modules/@purtuga/project-base/configs/webpack.config.js --env.build=prod.minified --env.build=prod.legacy.minified --env.build=prod.esm.minified",
 
-            "setup:dev": "node node_modules/project-base/scripts/create-dev",
+            "setup:dev": "node node_modules/@purtuga/project-base/scripts/create-dev",
 
-            "lint": "eslint -c ./node_modules/project-base/configs/eslint.config.js src/**/*.js",
-            "lint:fix": "eslint -c ./node_modules/project-base/configs/eslint.config.js --fix src/**/*.js"
+            "lint": "eslint -c ./node_modules/@purtuga/project-base/configs/eslint.config.js src/**/*.js",
+            "lint:fix": "eslint -c ./node_modules/@purtuga/project-base/configs/eslint.config.js --fix src/**/*.js"
         };
 
         writeFile(packageJsonFile, JSON.stringify(packageJson, null, 4)).then(() => {
